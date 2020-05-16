5a0b0f93026c34c6343011633d52e9f6a9a04a7c
vsanchez79 <65466871+vsanchez79@users.noreply.github.com>
2020-05-16 17:14:59 -0300

    Prueba la conxion con mysql

---
+++ probando_bd.php
@@ -0,0 +1,36 @@
+<?php echo "<h2> Hola mundo de los micro-servicios </h2>";
+
+
+$enlace = mysqli_connect("miapp-mysql", "devel", "s3cure", "bd_prueba","3306");
+//$enlace = mysqli_connect("127.0.0.1", "root", "", "bd_prueba");
+
+if (!$enlace) {
+    echo "Error: No se pudo conectar a MySQL." . PHP_EOL;
+    echo "errno de depuración: " . mysqli_connect_errno() . PHP_EOL;
+    echo "error de depuración: " . mysqli_connect_error() . PHP_EOL;
+    exit;
+}
+
+echo "Éxito: Se realizó una conexión apropiada a MySQL! La base de datos mi_bd es genial." . PHP_EOL;
+echo "Información del host: " . mysqli_get_host_info($enlace) . PHP_EOL;
+
+/* sentencia preparada */
+if ($sentencia = $enlace->prepare("SELECT * FROM tb_prueba")) {
+    $sentencia->execute();
+
+    /* vincular variables a la sentencia preparada */
+    $sentencia->bind_result($col1, $col2);
+
+    /* obtener valores */
+    while ($sentencia->fetch()) {
+		echo "<br>";
+        printf("%s %s\n", $col1, $col2);
+    }
+
+    /* cerrar la sentencia */
+    $sentencia->close();
+}
+/* cerrar la conexión */
+$enlace->close();
+
+?>
